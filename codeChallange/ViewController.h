5b97fbd883db1e01e6e5ffed73be93d20a1ce65b
Dmitry Krasulya <dkr@argus-soft.net>
2016-05-25 12:15:14 +0300

    Initial Commit

---
+++ codeChallange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  codeChallange
+//
+//  Created by Dima Krasulya on 5/25/16.
+//  Copyright © 2016 Argus Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
